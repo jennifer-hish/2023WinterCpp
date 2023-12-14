634d992e382de393ec01cc62bdd0b0c03fa260c9
Jhish <hish.jennifer@gmail.com>
2023-12-14 11:24:53 -0800

    2023-12-14 UI - text implementation function

--- BoxGame/BoxGame.cpp
+++ BoxGame/BoxGame.cpp
@@ -7,18 +7,19 @@
 //               reference: kbhit() to check if any key is pressed
 ////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
+#include <string>
 #include <conio.h>   // getch()
 #include "Point.hpp"
 #include "Map.hpp"
 
 void Set_Interface(int interface[], int w, int h);
 void Print_Interface(int interface[], int w, int h);
+void Text_Generation(int interface[], int x, int y, int const& section, std::string const& str);
 
 const int i_l=20, i_r=70, i_t=1, i_m=20, i_b=3;
 
 int main(void) {
   int interface[i_t+i_m+i_b+4][i_l+i_r+3] = {0};
-  std::cout << sizeof(interface) << ' ';
   Set_Interface(*interface, sizeof(interface[0])/sizeof(int), sizeof(interface)/sizeof(interface[0]));
   // Map map = Map(7,7);
   // std::cout << "\33c\e[3J" << map << std::endl;
@@ -53,7 +54,6 @@ int main(void) {
 }
 
 void Set_Interface(int interface[], int w, int h) {
-  std::cout << "set: " << w << ' ' << h << std::endl;
   for (int j=1; j<w-1; j++) {
     if (j == i_l+1) {
       interface[j] = 203;
@@ -87,43 +87,50 @@ void Set_Interface(int interface[], int w, int h) {
   interface[(i_t+i_m+3)*w-1] = 185;
 
   // menu text
-  int menu_line = 8, menu_left = 5;
-  interface[w*menu_line+menu_left] = 'M';
-  interface[w*menu_line+menu_left+1] = 'E';
-  interface[w*menu_line+menu_left+2] = 'N';
-  interface[w*menu_line+menu_left+3] = 'U';
-
-  interface[w*(menu_line+2)+menu_left] = 219;
-  interface[w*(menu_line+2)+menu_left+1] = '.';
-  interface[w*(menu_line+2)+menu_left+2] = 'T';
-  interface[w*(menu_line+2)+menu_left+3] = 'u';
-  interface[w*(menu_line+2)+menu_left+4] = 't';
-  interface[w*(menu_line+2)+menu_left+5] = 'o';
-  interface[w*(menu_line+2)+menu_left+6] = 'r';
-  interface[w*(menu_line+2)+menu_left+7] = 'i';
-  interface[w*(menu_line+2)+menu_left+8] = 'a';
-  interface[w*(menu_line+2)+menu_left+9] = 'l';
-
-  interface[w*(menu_line+3)+menu_left] = '2';
-  interface[w*(menu_line+3)+menu_left+1] = '.';
-  interface[w*(menu_line+3)+menu_left+2] = 'P';
-  interface[w*(menu_line+3)+menu_left+3] = 'l';
-  interface[w*(menu_line+3)+menu_left+4] = 'a';
-  interface[w*(menu_line+3)+menu_left+5] = 'y';
-
-  interface[w*(menu_line+4)+menu_left] = '3';
-  interface[w*(menu_line+4)+menu_left+1] = '.';
-  interface[w*(menu_line+4)+menu_left+2] = 'R';
-  interface[w*(menu_line+4)+menu_left+3] = 'a';
-  interface[w*(menu_line+4)+menu_left+4] = 'n';
-  interface[w*(menu_line+4)+menu_left+5] = 'k';
+  int menu_line = 8, menu_left = 4;
+  Text_Generation(interface, menu_left, menu_line, 'A', "MENU");
+  Text_Generation(interface, menu_left, menu_line+2, 'A', "1.Tutorial");
+  Text_Generation(interface, menu_left, menu_line+3, 'A', "2.Play");
+  Text_Generation(interface, menu_left, menu_line+4, 'A', "3.Rank");
+  Text_Generation(interface, 0, 0, 'D', "Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description");
 }
 void Print_Interface(int interface[], int w, int h) {
-  std::cout << "print: " << w << ' ' << h << std::endl;
+  std::cout << "\33c\e[3J";
   for (int i=0; i<h; i++) {
     for (int j=0; j<w; j++) {
       std::cout << char(interface[i*w+j]);
     }
     std::cout << std::endl;
+  }
+}
+void Text_Generation(int interface[], int x, int y, int const& section, std::string const& str) {
+  int max_length = i_r - 2;
+  switch (section) {
+    case 'A':
+      x += 1;
+      y += 1;
+      max_length = i_l - 2;
+      break;
+    case 'B':
+      x += i_l + 2;
+      y += 1;
+      break;
+    case 'C':
+      x += i_l + 2;
+      y += i_t + 2;
+      break;
+    case 'D':
+      x += i_l + 2;
+      y += i_t + i_m + 3;
+      break;
+    default:
+      std::cerr << "invalid section for Text_Generation()" << std::endl;
+      break;
+  }
+  int line = 0;
+  for (int i = 0; i < str.size(); ++i) {
+    if (i > 0 && i%max_length == 0)
+      line++;
+    interface[x+(y+line)*(i_l+i_r+3)+i-(max_length*line)+1] = str[i];
   }
 }
