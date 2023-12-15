325dd9d4c287f9d29cc6393035b8794efab4c8ac
Jhish <hish.jennifer@gmail.com>
2023-12-14 18:44:25 -0800

    2023-12-14 Menu control

--- BoxGame/BoxGame.cpp
+++ BoxGame/BoxGame.cpp
@@ -14,16 +14,51 @@
 
 void Set_Interface(int interface[], int w, int h);
 void Print_Interface(int interface[], int w, int h);
+void Update_Menu(int interface[], int n, bool selected);
 void Text_Generation(int interface[], int x, int y, int const& section, std::string const& str);
 
 const int i_l=20, i_r=70, i_t=1, i_m=20, i_b=3;
+const int menu_line = 10, menu_count = 4;
 
 int main(void) {
   int interface[i_t+i_m+i_b+4][i_l+i_r+3] = {0};
   Set_Interface(*interface, sizeof(interface[0])/sizeof(int), sizeof(interface)/sizeof(interface[0]));
+  // 메뉴 선택 조건
+  int current_menu = 1;
+  bool is_menu_selected = false;
+  // 게임 진행중 확인
+  bool is_game_running = false;
   // Map map = Map(7,7);
   // std::cout << "\33c\e[3J" << map << std::endl;
-  // char key;
+  char key;
+  Update_Menu(*interface, current_menu, is_menu_selected);
+  while (current_menu != menu_count || is_menu_selected == false) {
+    Print_Interface(*interface, sizeof(interface[0])/sizeof(int), sizeof(interface)/sizeof(interface[0]));
+    // key input
+    if (key = getch()) {
+      // std::cout << "key=" << key << std::endl;
+      
+      // menu control
+      if (is_menu_selected == false) {
+        if (key == 32 || key == 13) {
+          is_menu_selected = true;
+          Update_Menu(*interface, current_menu, is_menu_selected);
+        } else if (key == 'w' || key == 'W') {
+          if (current_menu > 1)
+            current_menu--;
+          Update_Menu(*interface, current_menu, is_menu_selected);
+        } else if (key == 's' || key == 'S') {
+          if (current_menu < menu_count)
+            current_menu++;
+          Update_Menu(*interface, current_menu, is_menu_selected);
+        }
+      } // else의 경우: 동작하지 않음, 그러나 임시로 동작시켰으므로 추후 코드 삭제
+      else {
+        if (key == 32 || key == 13)
+          is_menu_selected = false;
+      }
+    }
+  }
   // bool runGame = true;
   // while (runGame == true) {
   //   key = getch();
@@ -49,7 +84,6 @@ int main(void) {
   //     std::cout << "\33c\e[3J" << "GAME OVER!" << std::endl;
   //   }
   // }
-  Print_Interface(*interface, sizeof(interface[0])/sizeof(int), sizeof(interface)/sizeof(interface[0]));
   return 0;
 }
 
@@ -87,12 +121,13 @@ void Set_Interface(int interface[], int w, int h) {
   interface[(i_t+i_m+3)*w-1] = 185;
 
   // menu text
-  int menu_line = 8, menu_left = 4;
-  Text_Generation(interface, menu_left, menu_line, 'A', "MENU");
-  Text_Generation(interface, menu_left, menu_line+2, 'A', "1.Tutorial");
-  Text_Generation(interface, menu_left, menu_line+3, 'A', "2.Play");
-  Text_Generation(interface, menu_left, menu_line+4, 'A', "3.Rank");
-  Text_Generation(interface, 0, 0, 'D', "Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description Very Long Description");
+  int menu_left = 4;
+  Text_Generation(interface, menu_left, menu_line-2, 'A', "MENU");
+  Text_Generation(interface, menu_left, menu_line, 'A', "1.Tutorial");
+  Text_Generation(interface, menu_left, menu_line+1, 'A', "2.Play");
+  Text_Generation(interface, menu_left, menu_line+2, 'A', "3.Rank");
+  Text_Generation(interface, menu_left, menu_line+3, 'A', "4.Quit");
+  Text_Generation(interface, 0, 0, 'D', "JHISH 2023 Winter C++ Project");
 }
 void Print_Interface(int interface[], int w, int h) {
   std::cout << "\33c\e[3J";
@@ -100,7 +135,18 @@ void Print_Interface(int interface[], int w, int h) {
     for (int j=0; j<w; j++) {
       std::cout << char(interface[i*w+j]);
     }
-    std::cout << std::endl;
+    std::cout << '\n';
+  }
+}
+void Update_Menu(int interface[], int n, bool selected) {
+  for (int i=0; i<menu_count; ++i) {
+    if (i == n-1) {
+      interface[(menu_line+i+1)*(i_l+i_r+3)+3] = selected ? 219 : '[';
+      interface[(menu_line+i+1)*(i_l+i_r+3)+i_l-2] = selected ? 219 : ']';
+    } else {
+      interface[(menu_line+i+1)*(i_l+i_r+3)+3] = 32;
+      interface[(menu_line+i+1)*(i_l+i_r+3)+i_l-2] = 32;
+    }
   }
 }
 void Text_Generation(int interface[], int x, int y, int const& section, std::string const& str) {
@@ -124,7 +170,7 @@ void Text_Generation(int interface[], int x, int y, int const& section, std::str
       y += i_t + i_m + 3;
       break;
     default:
-      std::cerr << "invalid section for Text_Generation()" << std::endl;
+      std::cerr << "invalid section for Text_Generation()\n";
       break;
   }
   int line = 0;
